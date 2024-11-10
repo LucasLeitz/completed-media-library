d80d981787188d959e335518f24d52932b1d2b36
lucasleitz <lucasleitz@outlook.com>
2024-11-10 08:30:06 -0500

    First commit for new project

---
+++ src/main/java/com/example/completed_media_library/CompletedMediaLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.completed_media_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletedMediaLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompletedMediaLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/completed_media_library/CompletedMediaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.completed_media_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompletedMediaLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
